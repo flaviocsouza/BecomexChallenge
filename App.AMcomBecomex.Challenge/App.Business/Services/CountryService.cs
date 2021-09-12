6b64e5500e5839cba5be673354ebd30804741bed
Flavio Augusto Corrêa de Souza <flavio.augusto.c.souza@gmail.com>
2021-09-12 15:31:31 -0400

    Challenge

--- App.AMcomBecomex.Challenge/App.Business/Services/CountryService.cs
+++ App.AMcomBecomex.Challenge/App.Business/Services/CountryService.cs
@@ -42,51 +42,5 @@ namespace App.Business.Services
             _countryCacheService.SetCountry(country);
             return country;
         }
-
-        //public async Task<IEnumerable<Country>> GetLandRoute(string originCountry, string destinationCountryCode)
-        public async Task<int> GetLandRoute(string originCode, string destinationCode)
-        {
-            originCode = originCode.ToUpper();
-            destinationCode = destinationCode.ToUpper();
-
-            Queue<string> countriesQueue = new Queue<string>();
-            Stack<string> countriesStack = new Stack<string>();
-            List<string> visitedCountries = new List<string>();
-            LinkedList<string> countries = new LinkedList<string>();
-
-            countriesQueue.Enqueue(originCode);
-            visitedCountries.Add(originCode);
-            int counterTest = 0;
-            var current = countries.AddFirst(originCode);
-
-            while (countriesQueue.Count > 0)
-            {   
-                string currentCode = countriesQueue.Dequeue();
-                current = countries.Find(currentCode);
-                countriesStack.Push(currentCode);
-                counterTest++;               
-
-                IEnumerable<string> currentBorders = (await GetCountryByCode(currentCode)).borders;
-                IEnumerable<string> validBorders = currentBorders.Where(country => !visitedCountries.Contains(country)).ToList();
-                
-                foreach(string borderCode in validBorders)
-                {
-                    countries.AddAfter(current, borderCode);
-                    if (borderCode == destinationCode)
-                    {
-                        countriesStack.Push(borderCode);
-                        return counterTest;// retorno correto
-                    }
-                    countriesQueue.Enqueue(borderCode);
-                    visitedCountries.Add(borderCode);
-                }
-                if (!validBorders.Any()) {
-                    countries.Remove(currentCode);
-                    countriesStack.Pop();
-                } 
-            }
-
-            return -1;
-        }
     }
 }
